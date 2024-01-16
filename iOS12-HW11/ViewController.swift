86046e8c715c952a7c390e0fe4f4c852b61786d7
DenisMayshev <denmay13@mail.ru>
2024-01-16 17:50:03 +0400

    Initial Commit

---
+++ iOS12-HW11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS12-HW11
+//
+//  Created by Ден Майшев on 16.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
